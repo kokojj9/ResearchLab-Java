10e1cfa02ca7bf1bdc3e07da87dce6c3967b03e6
kokojj <kovjscl976@naver.com>
2024-06-14 23:08:05 +0900

    개발환경구성

---
+++ src/main/java/com/example/researchlab/ResearchlabApplication.java
@@ -0,0 +1,13 @@
+package com.example.researchlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResearchlabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResearchlabApplication.class, args);
+	}
+
+}
