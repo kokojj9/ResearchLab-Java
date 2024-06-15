d8cf77c8260659ef10120d9e5dae2770d27ab786
kokojj <kovjscl976@naver.com>
2024-06-15 18:21:34 +0900

    서버실행 테스트

--- src/main/java/com/example/researchlab/common/controller/TestController.java
+++ src/main/java/com/example/researchlab/common/controller/TestController.java
@@ -1,4 +1,10 @@
 package com.example.researchlab.common.controller;
 
+import org.apache.tomcat.util.net.TLSClientHelloExtractor;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
 public class TestController {
+
 }
