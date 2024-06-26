740481b659b3b18f52b28e8be479179a304012c7
kokojj <kovjscl976@naver.com>
2024-06-27 03:18:13 +0900

    공공데이터 요청 성공

--- src/main/java/com/example/researchlab/investment/controller/InvestmentController.java
+++ src/main/java/com/example/researchlab/investment/controller/InvestmentController.java
@@ -4,10 +4,7 @@ import com.example.researchlab.common.model.vo.ResponseData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -17,6 +14,9 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @RestController
 @RequestMapping("/investment")
@@ -26,29 +26,32 @@ public class InvestmentController {
     @Value("${SERVICE_KEY}")
     private String SERVICE_KEY;
 
-    @ResponseBody
-    @PostMapping(value = "/stock", produces = "application/json; charset=UTF-8")
-    public String getStockInfo(String stockName) throws IOException {
-        StringBuilder url = new StringBuilder();
+    @GetMapping(value = "/findStock", produces = "application/json; charset=UTF-8")
+    public String getStockInfo(@RequestParam String stockName) throws IOException {
+        String encodedStockName = URLEncoder.encode(stockName, "UTF-8");
 
-        url.append("https://apis.data.go.kr/1160100/service/GetStockSecuritiesInfoService/getStockPriceInfo");
-        url.append("?serviceKey").append(SERVICE_KEY);
-        url.append("&numOfRows=10");
-        url.append("&pageNo=1");
-        url.append("&likeItmsNm=").append(URLEncoder.encode(stockName, "UTF-8"));
-        url.append("&resultType=json");
+        String url = "https://apis.data.go.kr/1160100/service/GetStockSecuritiesInfoService/getStockPriceInfo"
+                   + "?serviceKey=" + SERVICE_KEY
+                   + "&numOfRows=10"
+                   + "&pageNo=1"
+                   + "&likeItmsNm=" + encodedStockName
+                   + "&resultType=json";
 
-        URL requestUrl = new URL(url.toString());
-
+        URL requestUrl = new URL(url);
         HttpURLConnection urlConnection = (HttpURLConnection) requestUrl.openConnection();
         urlConnection.setRequestMethod("GET");
-        BufferedReader br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-        String responseJson = br.readLine();
+        BufferedReader br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream(), "UTF-8")); // 인코딩 설정
+        StringBuilder responseJson = new StringBuilder();
+        String line;
+
+        while ((line = br.readLine()) != null) {
+            responseJson.append(line);
+        }
 
         br.close();
         urlConnection.disconnect();
 
-        return responseJson;
+        return responseJson.toString();
     }
 
 
