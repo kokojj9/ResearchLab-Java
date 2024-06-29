822701e7d849d411e592c8f552e4b136e1279d70
kokojj <kovjscl976@naver.com>
2024-06-30 01:47:54 +0900

    HttpUrl생성 메소드 분리

--- src/main/java/com/example/researchlab/investment/controller/InvestmentController.java
+++ src/main/java/com/example/researchlab/investment/controller/InvestmentController.java
@@ -15,8 +15,10 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
 
 @RestController
 @RequestMapping("/investment")
@@ -28,18 +30,11 @@ public class InvestmentController {
 
     @GetMapping(value = "/findStock", produces = "application/json; charset=UTF-8")
     public String getStockInfo(@RequestParam String stockName) throws IOException {
+        // 2일전 or 월,화 일 경우에는 금요일로 조회하도록
+        // 공공데이터 api는 2일전 종가로 조회 되기 때문에 추후에 고도화를 통해 바꿀 예정
         String encodedStockName = URLEncoder.encode(stockName, "UTF-8");
 
-        String url = "https://apis.data.go.kr/1160100/service/GetStockSecuritiesInfoService/getStockPriceInfo"
-                   + "?serviceKey=" + SERVICE_KEY
-                   + "&numOfRows=10"
-                   + "&pageNo=1"
-                   + "&likeItmsNm=" + encodedStockName
-                   + "&resultType=json";
-
-        URL requestUrl = new URL(url);
-        HttpURLConnection urlConnection = (HttpURLConnection) requestUrl.openConnection();
-        urlConnection.setRequestMethod("GET");
+        HttpURLConnection urlConnection = getHttpURLConnection(encodedStockName);
         BufferedReader br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream(), "UTF-8")); // 인코딩 설정
         StringBuilder responseJson = new StringBuilder();
         String line;
@@ -54,11 +49,19 @@ public class InvestmentController {
         return responseJson.toString();
     }
 
+    private HttpURLConnection getHttpURLConnection(String encodedStockName) throws IOException {
+        String url = "https://apis.data.go.kr/1160100/service/GetStockSecuritiesInfoService/getStockPriceInfo"
+                   + "?serviceKey=" + SERVICE_KEY
+                   + "&numOfRows=10"
+                   + "&pageNo=1"
+                   + "&likeItmsNm=" + encodedStockName
+                   + "&resultType=json";
 
-
-
-
-
+        URL requestUrl = new URL(url);
+        HttpURLConnection urlConnection = (HttpURLConnection) requestUrl.openConnection();
+        urlConnection.setRequestMethod("GET");
+        return urlConnection;
+    }
 
 
 }
