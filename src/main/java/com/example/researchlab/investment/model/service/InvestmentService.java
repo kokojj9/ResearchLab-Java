e2d53de6ae6ac02f90fb3a139e3f5ef97efae328
kokojj <kovjscl976@naver.com>
2024-06-30 09:55:13 +0900

    종목조회 비즈니스 로직 분리를 위한 서비스 클래스 생성

---
+++ src/main/java/com/example/researchlab/investment/model/service/InvestmentService.java
@@ -0,0 +1,7 @@
+package com.example.researchlab.investment.model.service;
+
+public interface InvestmentService {
+
+
+    
+}
---
+++ src/main/java/com/example/researchlab/investment/model/service/InvestmentServiceImpl.java
@@ -0,0 +1,11 @@
+package com.example.researchlab.investment.model.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class InvestmentServiceImpl implements InvestmentService{
+
+
+
+
+}
