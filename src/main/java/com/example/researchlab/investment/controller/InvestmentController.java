284b69a1f51f1047157b3b09d72fbd5857e1f437
kokojj <kovjscl976@naver.com>
2024-07-03 22:04:23 +0900

    유저 세팅 메소드 작업 시작

--- src/main/java/com/example/researchlab/investment/controller/InvestmentController.java
+++ src/main/java/com/example/researchlab/investment/controller/InvestmentController.java
@@ -1,11 +1,9 @@
 package com.example.researchlab.investment.controller;
 
 import com.example.researchlab.investment.model.service.InvestmentService;
+import com.example.researchlab.investment.model.vo.MyStockList;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.net.URLEncoder;
@@ -19,15 +17,18 @@ public class InvestmentController {
     @GetMapping(value = "/findStock", produces = "application/json; charset=UTF-8")
     public String getStockInfo(@RequestParam String stockName) throws IOException {
         // 여러날짜 데이터가 조회되어 가장 최근날짜 데이터만 필터링하는 로직은 프론트에서 구성함
-        // 공공데이터 api는 2일전 종가로 조회 되기 때문에 추후에 고도화를 통해 바꿀 예정
+        // 공공데이터 api는 2일전 종가로 조회 되기 때문에 추후에 다른 증권사api로 고도화를 통해 바꿀 예정
         String encodedStockName = URLEncoder.encode(stockName, "UTF-8");
 
-        investmentService.getStockInfo(encodedStockName);
-
-        return null;
+        return investmentService.getStockInfo(encodedStockName);
     }
 
-
+    @PostMapping("/insertSetting")
+    public String insertUserSetting(@RequestBody MyStockList stocklist){
+        // 사용자 설정 목록 저장 메소드
+        
+        return null;
+    }
 
 
 
---
+++ src/main/java/com/example/researchlab/investment/model/vo/MyStockList.java
@@ -0,0 +1,6 @@
+package com.example.researchlab.investment.model.vo;
+
+public class MyStockList {
+
+
+}
