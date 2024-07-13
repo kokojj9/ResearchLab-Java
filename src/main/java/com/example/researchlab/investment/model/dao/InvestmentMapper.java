d9676ff84ccb3a706597d428ffaacfe4699f33a4
kokojj <kovjscl976@naver.com>
2024-07-13 22:08:45 +0900

    저장과 조회로직 추가 및 테스트 코드로 검사

--- src/main/java/com/example/researchlab/investment/model/dao/InvestmentMapper.java
+++ src/main/java/com/example/researchlab/investment/model/dao/InvestmentMapper.java
@@ -1,12 +1,16 @@
 package com.example.researchlab.investment.model.dao;
 
+import com.example.researchlab.investment.model.vo.MyStockList;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.HashMap;
+import java.util.List;
 
 @Mapper
 public interface InvestmentMapper {
     int saveStockList(HashMap<String, Object> userStockList);
 
     int saveStockListItem(HashMap<String, Object> userStockList);
+
+    List<MyStockList> getStockList(String memberNo);
 }
--- src/main/java/com/example/researchlab/investment/model/service/InvestmentService.java
+++ src/main/java/com/example/researchlab/investment/model/service/InvestmentService.java
@@ -1,12 +1,15 @@
 package com.example.researchlab.investment.model.service;
 
+import com.example.researchlab.investment.model.vo.MyStockList;
+
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.util.HashMap;
+import java.util.List;
 
 public interface InvestmentService {
 
     String getStockInfo(String stockName) throws IOException;
     HttpURLConnection getHttpURLConnection(String encodedStockName) throws IOException;
-    int saveStockList(HashMap<String, Object> stocklist);
+    List<MyStockList> saveStockList(HashMap<String, Object> stocklist);
 }
