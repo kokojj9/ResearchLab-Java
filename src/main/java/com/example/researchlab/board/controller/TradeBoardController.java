afd5ec8ac1770edfa3221116de702f02fab5b0c2
kokojj <kovjscl976@naver.com>
2024-07-31 17:01:25 +0900

    조회 기능 페이징 처리

--- src/main/java/com/example/researchlab/board/controller/TradeBoardController.java
+++ src/main/java/com/example/researchlab/board/controller/TradeBoardController.java
@@ -8,20 +8,13 @@ import com.example.researchlab.common.model.vo.ResponseData;
 import com.example.researchlab.template.ResponseTemplate;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -34,6 +27,10 @@ public class TradeBoardController {
     private final BoardFileService boardFileService;
 
     // 글 조회
+    @GetMapping
+    public ResponseEntity<ResponseData<Object>> selectTradePosts(){
+        return null;
+    }
     // 글 쓰기
     @PostMapping("/posts")
     public ResponseEntity<ResponseData<Object>> saveTradePost(@RequestPart("tradePost") TradePost tradePost,
@@ -45,18 +42,7 @@ public class TradeBoardController {
             rd = responseTemplate.fail("작성 실패", HttpStatus.BAD_REQUEST);
         } else {
             if(images != null){
-                List<PostImage> imageList = new ArrayList<>();
-
-                for (MultipartFile image : images) {
-                    if (!image.isEmpty()) {
-                        String storedFileName = boardFileService.saveFile(session, image);
-                        PostImage postImage = new PostImage();
-                        postImage.setOriginalName(image.getOriginalFilename());
-                        postImage.setStoredName(storedFileName);
-                        imageList.add(postImage);
-                    }
-                }
-                tradePost.setImageList(imageList);
+                tradePost.setImageList(setImages(session, images));
             }
 
             int result = tradeBoardService.saveTradePost(tradePost);
@@ -64,7 +50,22 @@ public class TradeBoardController {
             rd = result > 0 ? responseTemplate.success("작성 성공", null, HttpStatus.OK) :
                               responseTemplate.fail("작성 실패", HttpStatus.BAD_REQUEST);
         }
+
         return rd;
+    }
+
+    private List<PostImage> setImages(HttpSession session, List<MultipartFile> images) throws IOException {
+        List<PostImage> imageList = new ArrayList<>();
+        for (MultipartFile image : images) {
+            if (!image.isEmpty()) {
+                String storedFileName = boardFileService.saveFile(session, image);
+                PostImage postImage = new PostImage();
+                postImage.setOriginalName(image.getOriginalFilename());
+                postImage.setStoredName(storedFileName);
+                imageList.add(postImage);
+            }
+        }
+        return imageList;
     }
 
     // 상세 조회
