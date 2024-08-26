224759740f11ebdb2edd2b7d103f7a85c8ca2c44
kokojj <kovjscl976@naver.com>
2024-08-27 02:07:09 +0900

    [ feature ] 테스트 코드 작성
    1. 리팩토링한 로그인 메서드 테스트 코드 작성

--- src/main/java/com/example/researchlab/member/controller/MemberController.java
+++ src/main/java/com/example/researchlab/member/controller/MemberController.java
@@ -27,45 +27,6 @@ public class MemberController {
     public ResponseData<Object> login(@RequestBody @Valid Member member, BindingResult br) {
         logger.info("회원 로그인 시도: {}", member.getMemberId());
 
-<<<<<<< Updated upstream
-        if (br.hasErrors()) {
-            logger.warn("유효성 검사 오류: {}", br.getAllErrors());
-            return responseTemplate.fail("잘못된 정보", HttpStatus.BAD_REQUEST);
-        } else {
-            Member loginMember = memberService.login(member);
-
-            if (loginMember != null) {
-                loginMember.setMemberPwd(null);
-                session.setAttribute("loginMember", loginMember);
-                logger.info("로그인 성공: {}", member.getMemberId());
-                return responseTemplate.success("로그인 성공", null, HttpStatus.OK);
-            }
-
-            logger.warn("로그인 실패: {}", member.getMemberId());
-            return responseTemplate.success("존재하지 않는 회원", null, HttpStatus.OK);
-        }
-    }
-
-    @GetMapping("/getSession")
-    public ResponseEntity<ResponseData<Object>> getSessionInfo(HttpSession session) {
-        Member member = (Member) session.getAttribute("loginMember");
-
-        if (member != null) {
-            logger.info("유효한 세션: {}", member.getMemberId());
-            return responseTemplate.success("유효한 세션", member, HttpStatus.OK);
-        }
-        logger.warn("세션 없음");
-        return responseTemplate.success("세션 없음", null, HttpStatus.OK);
-    }
-
-    @PostMapping("/logout")
-    public ResponseEntity<ResponseData<Object>> logout(HttpSession session) {
-        logger.info("로그아웃");
-        session.invalidate();
-        return responseTemplate.success("로그아웃 성공", null, HttpStatus.OK);
-    }
-
-=======
         ResponseData.ResponseDataBuilder<Object> responseDataBuilder = ResponseData.builder();
 
         if (br.hasErrors()) {
@@ -95,23 +56,6 @@ public class MemberController {
     @PostMapping("/enroll")
     public ResponseData<Object> enrollMember(@RequestBody @Valid Member member, BindingResult br) {
         logger.info("회원가입 시도: {}", member.getMemberId());
-<<<<<<< Updated upstream
-
-        if (br.hasErrors()) {
-            logger.warn("유효성 검사 오류: {}", br.getAllErrors());
-            return responseTemplate.fail("잘못된 정보", HttpStatus.BAD_REQUEST);
-        }
-
-        int result = memberService.enrollMember(member);
-
-        if (result > 0) {
-            logger.info("회원가입 성공: {}", member.getMemberId());
-            return responseTemplate.success("회원가입 성공", null, HttpStatus.OK);
-        }
-
-        logger.warn("회원가입 실패: {}", member.getMemberId());
-        return responseTemplate.success("회원가입 실패", null, HttpStatus.OK);
-=======
         ResponseData.ResponseDataBuilder<Object> responseDataBuilder =  ResponseData.builder();
 
         if (br.hasErrors()) {
@@ -136,6 +80,5 @@ public class MemberController {
         }
 
         return responseDataBuilder.build();
->>>>>>> Stashed changes
     }
 }
