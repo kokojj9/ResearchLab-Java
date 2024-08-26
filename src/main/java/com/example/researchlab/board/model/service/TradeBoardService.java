617ddb0d97e56467f754584ba83c4c623520fd5c
kokojj <kovjscl976@naver.com>
2024-08-27 02:25:28 +0900

    [ feature ] 테스트 코드 작성
    1. 리팩토링한 로그인 메서드 테스트 코드 작성

--- src/main/java/com/example/researchlab/board/model/service/TradeBoardService.java
+++ src/main/java/com/example/researchlab/board/model/service/TradeBoardService.java
@@ -1,7 +1,10 @@
 package com.example.researchlab.board.model.service;
 
 import com.example.researchlab.board.model.vo.Post;
+import com.example.researchlab.common.model.vo.ResponseData;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 
 public interface TradeBoardService {
     int saveTradePost(Post tradePost);
